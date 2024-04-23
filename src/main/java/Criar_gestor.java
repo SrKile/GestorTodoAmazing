8ea06b01e0e46f96c1d374a8bb3129d72948a314
degor <diogogomes600@gmail.com>
2024-04-23 13:46:37 +0100

    Criar_login_gestor

--- src/main/java/Criar_gestor.java
+++ src/main/java/Criar_gestor.java
@@ -1,24 +1,29 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Criar_gestor {
+public class Criar_gestor extends JFrame {
     private JPanel panel1;
-    private JTextField gestorAdminTextField;
-    private JButton listaDeAlunosEButton;
-    private JButton gerirAulasButton1;
-    private JButton gerirExamesButton;
-    private JButton gerirAulasButton;
+    private JTextField Nome;
+    private JTextField Curriculo;
+    private JTextField numCC;
     private JButton voltarButton;
+    private JButton Criar;
 
     public Criar_gestor() {
-        voltarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-        voltarButton.addActionListener(new ActionListener() {
-        });
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(panel1);
+        pack();
+        voltarButton.addActionListener(this::btnVoltarActionPerformed);
+        Criar.addActionListener(this::btnCriarPerformed);
     }
+
+    private void btnVoltarActionPerformed(ActionEvent actionEvent) {
+        // adicionar metoodo para voltar a janela anterior
+    }
+
+    private void btnCriarPerformed(java.awt.event.ActionEvent evt) {
+        var janela = new LoginGestor2();
+        janela.setVisible(true); // se não foi executado no construtor…
+    }
+
 }
--- src/main/java/LoginGestor2.java
+++ src/main/java/LoginGestor2.java
@@ -1,8 +1,28 @@
 import javax.swing.*;
-
-public class LoginGestor2 {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class LoginGestor2 extends JFrame {
     private JButton entrarButton;
     private JButton criarButton;
     private JTextField textField1;
     private JPasswordField passwordField1;
+    private JPanel panel1;
+
+    public LoginGestor2() {
+        setContentPane(panel1);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+
+        criarButton.addActionListener(this::btnCriarPerformed);
+    }
+
+
+    private void btnCriarPerformed(java.awt.event.ActionEvent evt) {
+        var janela = new LoginGestor2();
+        janela.setVisible(true); // se não foi executado no construtor…
+    }
+
 }
