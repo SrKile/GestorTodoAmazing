f4e88df00e503b8f4cfdf8e999faecfba45d2852
degor <diogogomes600@gmail.com>
2024-04-23 12:03:20 +0100

    Login_geestor_criar_gestor

---
+++ src/main/java/Criar_gestor.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Criar_gestor {
+    private JPanel panel1;
+    private JTextField gestorAdminTextField;
+    private JButton listaDeAlunosEButton;
+    private JButton gerirAulasButton1;
+    private JButton gerirExamesButton;
+    private JButton gerirAulasButton;
+    private JButton voltarButton;
+
+    public Criar_gestor() {
+        voltarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        voltarButton.addActionListener(new ActionListener() {
+        });
+    }
+}
