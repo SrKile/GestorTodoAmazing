5f9c9565c08e7715544e318789836f65a9319c25
degor <diogogomes600@gmail.com>
2024-04-23 12:12:14 +0100

    add_logingestor2_remove_logingestor

---
+++ src/main/java/LoginGestor2.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class LoginGestor2 {
+    private JButton entrarButton;
+    private JButton criarButton;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+}
